dce9131c65c22bfce7f7d0fcb66777c946899408
Guilherme <gui.osantos99@gmail.com>
2023-08-27 22:46:46 -0400

    Criação de Código Exercício 1
    Criação de Código Exercício 1

---
+++ Atividade 03/Atividade3 - Exercício1/src/App.java
@@ -0,0 +1,9 @@
+
+
+import dominio.escola.Aluno;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Aluno a1 = new Aluno(0, null, null, null, null, null, null, null, null, null);
+    }
+}
